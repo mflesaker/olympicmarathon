c9ca155ab51cfad4dc1367ba76f22ce6161a2ce4
Michelle Flesaker <mflesaker@smith.edu>
2022-03-22 12:36:53 -0400

    fixed situation so that what was a function is now just data

---
+++ R/data.R
@@ -0,0 +1,4 @@
+#' This file contains data from 2020 Tokyo Olympics Women's Marathon!
+#' @docType data
+"womens_2020_data"
+
---
+++ tests/testthat/test_womens2020.R
@@ -0,0 +1,3 @@
+testthat::test_that("the first rank is 1", {
+  testthat::expect_equal(womens_2020_data[[1, 1]], "1")
+})
