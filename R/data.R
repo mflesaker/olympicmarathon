ce79673dd284860c9705b410cae3bc245b52dc7e
Georgia Gans <ggans@smith.edu>
2022-03-23 08:54:10 -0400

    Added documentation for womens_2020_data and mens_2020_data

--- R/data.R
+++ R/data.R
@@ -1,7 +1,39 @@
-#' This file contains data from 2020 Tokyo Olympics Women's Marathon!
+#' @title Women's 2020 Olympic marathon data
 #' @docType data
+#' @description
+#' This file contains data from the 2020 Tokyo Olympics Women's Marathon.
+#' @usage womens_2020_data
+#' @format An object of class tbl_df (inherits from tbl, data.frame) with 88 rows and 9 columns:
+#' \describe{
+#' \item{rank}{Finishing place based on race time}
+#' \item{name}{Competitor's Name}
+#' \item{nationality}{3 letter abbreviation of the country that the competitor is representing}
+#' \item{result}{The competitor's finishing time (hr:min:sec) (DNF if did not finish)}
+#' \item{gender}{Gender of the competitor (F = female, M = male)}
+#' \item{event}{The Olympic event that the entry corresponds to}
+#' \item{location}{The location of the Olympic Games}
+#' \item{year}{The year of the Olympic Games}
+#' \item{medal}{What medal the competitor earned, if any}
+#' }
+#' @examples womens_2020_data
 "womens_2020_data"
 
-#' This file contains data from 2020 Tokyo Olympics Men's Marathon!
+#' @title Men's 2020 Olympic marathon data
 #' @docType data
+#' @description
+#' This file contains data from the 2020 Tokyo Olympics Men's Marathon.
+#' @usage mens_2020_data
+#' @format An object of class tbl_df (inherits from tbl, data.frame) with 106 rows and 9 columns:
+#' \describe{
+#' \item{rank}{Finishing place based on race time}
+#' \item{name}{Competitor's Name}
+#' \item{nationality}{3 letter abbreviation of the country that the competitor is representing}
+#' \item{result}{The competitor's finishing time (hr:min:sec) (DNF if did not finish)}
+#' \item{gender}{Gender of the competitor (F = female, M = male)}
+#' \item{event}{The Olympic event that the entry corresponds to}
+#' \item{location}{The location of the Olympic Games}
+#' \item{year}{The year of the Olympic Games}
+#' \item{medal}{What medal the competitor earned, if any}
+#' }
+#' @examples mens_2020_data
 "mens_2020_data"
