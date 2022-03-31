9a813a253c1b506f1a2ea8beea57a591858d7557
Michelle Flesaker <mflesaker@smith.edu>
2022-03-31 11:41:21 -0400

    merged documentation, added 2016 data

--- R/data.R
+++ R/data.R
@@ -1,9 +1,9 @@
-#' @title Women's 2020 Olympic marathon data
+#' @title Olympic Marathon Data by Gender and Year
 #' @docType data
 #' @description
-#' This file contains data from the 2020 Tokyo Olympics Women's Marathon.
+#' `womens_2020_data` contains data from the 2020 Tokyo Olympics Women's Marathon.
 #' @usage womens_2020_data
-#' @format An object of class tbl_df (inherits from tbl, data.frame) with 88 rows and 9 columns:
+#' @format `womens_2020_data` is an object of class tbl_df (inherits from tbl, data.frame) with 88 rows and 9 columns:
 #' \describe{
 #' \item{rank}{Finishing place based on race time}
 #' \item{name}{Competitor's Name}
@@ -18,22 +18,20 @@
 #' @examples womens_2020_data
 "womens_2020_data"
 
-#' @title Men's 2020 Olympic marathon data
+#' @rdname womens_2020_data
 #' @docType data
 #' @description
-#' This file contains data from the 2020 Tokyo Olympics Men's Marathon.
+#' `mens_2020_data` contains data from the 2020 Tokyo Olympics Men's Marathon.
 #' @usage mens_2020_data
-#' @format An object of class tbl_df (inherits from tbl, data.frame) with 106 rows and 9 columns:
-#' \describe{
-#' \item{rank}{Finishing place based on race time}
-#' \item{name}{Competitor's Name}
-#' \item{nationality}{3 letter abbreviation of the country that the competitor is representing}
-#' \item{result}{The competitor's finishing time (hr:min:sec) (DNF if did not finish)}
-#' \item{gender}{Gender of the competitor (F = female, M = male)}
-#' \item{event}{The Olympic event that the entry corresponds to}
-#' \item{location}{The location of the Olympic Games}
-#' \item{year}{The year of the Olympic Games}
-#' \item{medal}{What medal the competitor earned, if any}
-#' }
+#'
 #' @examples mens_2020_data
 "mens_2020_data"
+
+#' @rdname womens_2020_data
+#' @docType data
+#' @description
+#' `mens_2016_data` contains data from the 2016 Rio de Janeiro Olympics Men's Marathon
+#' @usage mens_2016_data
+#'
+#' @examples mens_2016_data
+"mens_2016_data"
