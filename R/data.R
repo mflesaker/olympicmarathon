d1765272d9eaa4ddb2e777fd3eec8aee2cf7279c
Michelle Flesaker <mflesaker@smith.edu>
2022-03-31 12:33:25 -0400

    added documentation change for data

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' @description
 #' `womens_2020_data` contains data from the 2020 Tokyo Olympics Women's Marathon
 #' @usage womens_2020_data
-#' @format `womens_2020_data` is an object of class tbl_df (inherits from tbl, data.frame) with 88 rows and 9 columns:
+#' @format `womens_2020_data` is an object of class tbl_df (inherits from tbl, data.frame) with 88 rows and 9 columns
 #' \describe{
 #' \item{rank}{Finishing place based on race time}
 #' \item{name}{Competitor's Name}
@@ -24,6 +24,7 @@
 #' @description
 #' `mens_2020_data` contains data from the 2020 Tokyo Olympics Men's Marathon
 #' @usage mens_2020_data
+#' @format `mens_2020_data` is an object of class tbl_df (inherits from tbl, data.frame) with 106 rows and 9 columns
 #'
 #' @examples mens_2020_data
 #' @source `mens_2020_data` comes from the [World Athletics website's 2020 men's marathon results page](https://www.worldathletics.org/results/olympic-games/2021/the-xxxii-olympic-games-athletics-7132391/men/marathon/final/result)
@@ -34,6 +35,7 @@
 #' @description
 #' `womens_2016_data` contains data from the 2016 Rio de Janeiro Olympics Women's Marathon
 #' @usage womens_2016_data
+#' @format `womens_2016_data` is an object of class tbl_df (inherits from tbl, data.frame) with 157 rows and 9 columns
 #'
 #' @examples womens_2016_data
 #' @source `womens_2016_data` comes from the [World Athletics website's 2016 women's marathon results page](https://www.worldathletics.org/results/olympic-games/2016/the-xxxi-olympic-games-7093747/women/marathon/final/result)
@@ -44,6 +46,7 @@
 #' @description
 #' `mens_2016_data` contains data from the 2016 Rio de Janeiro Olympics Men's Marathon
 #' @usage mens_2016_data
+#' @format `mens_2016_data` is an object of class tbl_df (inherits from tbl, data.frame) with 155 rows and 9 columns
 #'
 #' @examples mens_2016_data
 #' @source `mens_2016_data` comes from the [World Athletics website's 2016 men's marathon results page](https://www.worldathletics.org/results/olympic-games/2016/the-xxxi-olympic-games-7093747/men/marathon/final/result)
