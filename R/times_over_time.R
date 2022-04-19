d92b14663f91928aadc5f846f328034190a70b07
Michelle Flesaker <mflesaker@smith.edu>
2022-04-19 11:54:09 -0400

    gave the plot a title

--- R/times_over_time.R
+++ R/times_over_time.R
@@ -18,6 +18,7 @@ times_over_time <- function() {
     dplyr::mutate(result = hms::as_hms(result)) %>%
     ggplot2::ggplot(ggplot2::aes(x = year, y = result, color = gender)) +
     ggplot2::geom_jitter() +
+    ggplot2::ggtitle("All Finishing Times By Olympic Year") +
     ggplot2::scale_y_time("Result (hr:min:sec)", limits = c(hms::as_hms("02:00:00"), hms::as_hms("04:25:00"))) +
     ggplot2::scale_x_discrete("Olympic Year") +
     ggplot2::theme_minimal()
