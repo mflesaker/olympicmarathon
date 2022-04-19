3ebab524c8d3a6baaff9770c4c4fefc5795f6dd2
Michelle Flesaker <mflesaker@smith.edu>
2022-04-19 11:20:09 -0400

    dependencies and fixed eerror

--- R/times_over_time.R
+++ R/times_over_time.R
@@ -1,3 +1,5 @@
+utils::globalVariables(c("olympic_marathon", "result", "year", "gender"))
+
 ## ggplot visualization of how the finishing times of any ranking (places 1-10) have evolved over time for a given gender
 
 #' @export
@@ -6,7 +8,7 @@ times_over_time <- function() {
   olympic_marathon %>%
     dplyr::filter(!(result %in% c("DNF", "DNS", "DQ"))) %>%
     dplyr::mutate(result = hms::as_hms(result)) %>%
-    ggplot2::ggplot(aes(x = year, y = result, color = gender)) +
+    ggplot2::ggplot(ggplot2::aes(x = year, y = result, color = gender)) +
     ggplot2::geom_jitter() +
     ggplot2::scale_y_time(limits = c(hms::as_hms("02:00:00"), hms::as_hms("04:00:00"))) +
     ggplot2::theme_minimal()
