34f905f1469178e92a28187822a8351754471e97
Georgia Gans <ggans@smith.edu>
2022-04-21 11:36:24 -0400

    Added URL to hexSticker

--- data-raw/sticker.R
+++ data-raw/sticker.R
@@ -9,7 +9,9 @@ olympicmarathon_sticker <- sticker(imgurl,
         p_x = 1, p_y = 1.4,
         p_size = 5, s_x = 0.95, s_y = .8, s_width = 0.7, s_height = 0.4, h_size = 2, u_size = 4,
         filename = here::here("data-raw", "hex_olympicmarathon.png"),
-        white_around_sticker = TRUE)
+        white_around_sticker = TRUE) +
+  geom_url(url = "https://github.com/mflesaker/olympicmarathon",
+           size = 1.0)
 
 
 
